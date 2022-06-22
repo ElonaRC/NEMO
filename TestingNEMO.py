de612d9d5ac941f5d183463abe1970e020059715
ElonaRC <z5335164@ad.unsw.edu.au>
2022-06-22 16:40:26 +1000

    re100_SWH

--- TestingNEMO.py
+++ TestingNEMO.py
@@ -69,6 +69,42 @@ import nemo
 from nemo import scenarios
 #context is a class. We are setting up a default context  object? 
 c = nemo.Context()
-scenarios.re100SWH(c)
+#scenarios.re100SWH(c)
 scenarios.re100SWH_batteries(c)
+nemo.run(c)
+
+## Adding in generators for solar and wind into the same polygon. One polygon for each state for now. 
+## Capacity is set at 30GW for each poly. 
+
+#PV QLD in poly 4
+c.generators[4].set_capacity(30)
+#PV NSW in poly 30
+c.generators[30].set_capacity(30)
+#PV VIC in poly 37
+c.generators[37].set_capacity(30)
+#PV TAS in poly 41
+c.generators[41].set_capacity(30)
+#PV SA in poly 19
+c.generators[19].set_capacity(30)
+
+#WIND QLD in poly 4
+c.generators[47].set_capacity(30)
+#WIND NSW in poly 30
+c.generators[73].set_capacity(30)
+#WIND VIC in poly 37
+c.generators[80].set_capacity(30)
+#WIND TAS in poly 41
+c.generators[84].set_capacity(30)
+#WIND SA in poly 19
+c.generators[62].set_capacity(30)
+
+
+
+
 print(c.generators)
+
+from matplotlib.pyplot import ioff
+from datetime import datetime
+ioff()
+utils.plt.rcParams["figure.figsize"] = (12, 6)  # 12" x 6" figure
+utils.plot(c, xlim=[datetime(2010, 1, 5), datetime(2010, 1, 12)])
