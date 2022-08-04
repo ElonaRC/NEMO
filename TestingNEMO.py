b8b42d81aeec7613ed56b3f4f5fe00a27d31f578
ElonaRC <z5335164@ad.unsw.edu.au>
2022-08-05 09:42:13 +1000

    Spilled dataframe

--- TestingNEMO.py
+++ TestingNEMO.py
@@ -1,41 +1,35 @@
 ####-------------------- SET WD() -----------------------#####
 import os
-
-from nemo import polygons 
 os.chdir('/Users/elonarey-costa/Documents/phdCode/NEMO')
 
 ####---------------- EVOLVE OPTIMISER -------------------#####
 
-#### To run Evolve in terminal, navigate to the working directory /phdCode/NEMO and then:
+''' To run Evolve in terminal, navigate to the working directory /phdCode/NEMO and then:
 #python3 evolve ..... 
 
-#This creates an Evolve Icon and saves it on my desk top. The idea is to be able to double click on this icon, 
-#and it opens up the evolve optimiser 
+This creates an Evolve Icon and saves it on my desk top. The idea is to be able to double click on this icon, 
+and it opens up the evolve optimiser 
     #from pyshortcuts import make_shortcut
     #make_shortcut("/Users/elonarey-costa/Documents/phdCode/NEMO/evolve.py", name = 'Evolve', icon= '/Users/elonarey-costa/Documents/phdCode/NEMO/myicon.ico')
-
+Currently works but just spits out the default. 
+'''
 
 ####-------------------- Test CASE  ---------------------#####
 ## Create a test scenario that only includes: Solar, Wind, PumpedHydro, Hydro, Batteries in that merit order 
 ## This scenario has been created in scenario.py
 ## I can run this scenario in this script. 
 
-#start by importing nemo and relevant info (scenarios, generators etc)
+#start by importing nemo and relevant info (scenarios, generators etc that I want to use )
 
 import nemo
 from nemo import scenarios
 from nemo.generators import PV, Battery, PV1Axis
 
-
-#Set up an empty context class
+#Always set up an empty context class
 c = nemo.Context()
 
-
-#Select my chosen scenario
-#Just SWH
+#Select chosen scenario
 scenarios.re100SWH(c)
-#SWH + battery
-#scenarios.re100_batteries(c)
 
 ## Adding in generators for solar and wind into the same polygon. 
 ## One polygon for each state.
@@ -97,7 +91,7 @@ utils.plot(c, xlim=[datetime(2010, 1, 5), datetime(2010, 1, 12)])
 
 
 
-####-------------------- Test CASE  ---------------------#####
+####-------------------- Run re100SWH and map the outcome for one iteration ---------------------#####
 import nemo
 from nemo import scenarios
 
@@ -121,7 +115,86 @@ utils.plt.rcParams["figure.figsize"] = (12, 6)  # 12" x 6" figure
 utils.plot(c, xlim=[datetime(2010, 1, 5), datetime(2010, 1, 12)])
 
 
+####-------------------- Commands ---------------------#####
+'''
+EVOLVE Optimiser 
+In terminal widow -s = scenario -g = generators
+
+python3 evolve -s re100SWH -g 3 #Runs the scenario 
+python3 evolve -s re100SWH -g 3 > erc.txt #> erc.txt saves output. 
+
+'''
 
 
-#in terminal widow -s = scenario -g = generators and > erc.txt saves output. 
-python3 evolve -s ccgt -g 3 > erc.txt
+'''
+SAVING OUTPUT FROM EVOLVE 
+
+cat > re100SWHrun1  #saves output from evolve into a file called re100SWHrun1
+python3 summary < re100SWHrun1 #gives summary output in a nice table 
+
+Table like below:
+tech	          GW	share	  TWh	share	CF
+          PV	36.0	0.480	111.9	0.575	0.355
+        wind	32.8	0.438	 68.9	0.354	0.240
+       hydro	 3.9	0.052	  9.6	0.049	0.278
+         PSH	 2.2	0.030	  4.3	0.022	0.221
+     surplus     N/A	  N/A	 58.7	0.287
+'''
+
+
+'''
+REPLAY RESULTS
+
+python3 replay -f results.json  #gives us the saved results of the last optimisation run so I can go back to it. 
+python3 replay -f results.json -v   #gives us more saved results broken down more. 
+
+Replay GUI box: (after running evolve first)
+/usr/local/bin/python3.9 replay #gives us the replay gui box to print the results in a graph. 
+'''
+
+
+'''
+GIT SYNC FROM BEN MASTER UPSTREAM 
+
+In terminal:
+
+git pull upstream
+git merge upstream/master
+'''
+
+
+
+#create dataframe that saves the spilled energy for each time step for each generator
+#Do the same for spilled energy at each generator. 
+import nemo
+from nemo import scenarios 
+import numpy as np 
+import pandas as pd
+c = nemo.Context()
+scenarios.re100SWH(c)
+nemo.run(c)
+
+#create an array of all the generators and their power output for each time step. 
+len(c.generators)
+power = np.zeros((137, 8760))
+for i, g in enumerate(c.generators):
+    power[i] = list(g.series_power.values())
+np.savetxt("powerTest.csv", power, delimiter=",")
+
+#create an array of all the spilled energy for each time step. 
+spills = np.zeros((137, 8760))
+for i, g in enumerate(c.generators):
+    spills[i] = list(g.series_spilled.values())
+np.savetxt("spillsTest.csv", spills, delimiter=",")
+
+
+
+print(c.generators[0])
+print(c.generators[0].series_spilled) #print time series of energy spilled for generator 0 (PV in this case)
+
+
+
+
+
+
+#To consider: one battery per state and make them discharge over night as peakers 
