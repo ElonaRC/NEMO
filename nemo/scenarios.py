085c6e2f6ff39e6637e85c873a6e9f00431c52d7
ElonaRC <z5335164@ad.unsw.edu.au>
2024-02-22 16:20:52 +1100

    Update scenarios.py

--- nemo/scenarios.py
+++ nemo/scenarios.py
@@ -7,6 +7,7 @@
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
+
 """Supply side scenarios."""
 
 from nemo import configfile, regions
